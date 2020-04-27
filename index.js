d4584b727fbfd5178131981af49dd961a4e01bce
LeoPuertas <leopuertas01@gmail.com>
2020-04-27 13:36:21 -0300

    Add yargs

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const https = require('https');
 
-const argv = require('yargs').argv
+const yargs = require('yargs');
 
-if (!argv.currencyType && !argv.value) {
-    console.log('O for Dollar Oficial');
-    console.log('B for Dollar Blue');
-    console.log('E for Euro');
-    console.log('R for Real');
-    console.log('P for Peso');
-    throw new Error('CurrencyType and value are required')
-}
+var argv = yargs.option('currency', {
+    alias: 'c',
+    describe: `Choose the currency type
+               'O' for Dollar Oficial
+               'B' for Dollar Blue
+               'E' for Euro
+               'R' for Real
+               'P' for Peso`,
+    choices: ['O', 'B', 'E', 'R', 'P'],
+    default: 'P'
+})
+    .option('value', {
+        alias: 'v',
+        describe: 'value to convert',
+        default: 100
+    })
+    .option('format', {
+        alias: 'f',
+        describe: `Response format
+                   T for Table
+                   J for Json
+                   C for Exchange Rate`,
+        choises: ['T', 'J', 'C'],
+        default: 'J'
+    })
+    .argv;
 
-const currencyType = argv.currencyType;
+const currencyType = argv.currency;
 const inputValue = argv.value;
+const printFormat = argv.format;
 
 const DOLAR_URL = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales';
 const REAL_URL = 'https://www.dolarsi.com/api/api.php?type=genedolar&opc=ah';
@@ -24,7 +43,7 @@ let euro = 0;
 let real = 0;
 let peso = 0;
 
-const dolarValue = new Promise((resolve, reject) => {
+const getDolar = new Promise((resolve, reject) => {
     https.get(DOLAR_URL, (res) => {
         res.on('data', (data) => {
             let resp = JSON.parse(data);
@@ -45,7 +64,7 @@ const dolarValue = new Promise((resolve, reject) => {
     });
 });
 
-const euroValue = new Promise((resolve, reject) => {
+const getEuro = new Promise((resolve, reject) => {
     https.get(EURO_URL, (res) => {
         res.on('data', (data) => {
             let resp = JSON.parse(data);
@@ -57,7 +76,7 @@ const euroValue = new Promise((resolve, reject) => {
     });
 });
 
-const realValue = new Promise((resolve, reject) => {
+const getReal = new Promise((resolve, reject) => {
     https.get(REAL_URL, (res) => {
         res.on('data', (data) => {
             let resp = JSON.parse(data);
@@ -69,7 +88,7 @@ const realValue = new Promise((resolve, reject) => {
     });
 });
 
-let pesoValue = (currency, value) => {
+let getPeso = (currency, value) => {
     switch (currency) {
         case 'O':
             return officialDolar * value;
@@ -84,21 +103,57 @@ let pesoValue = (currency, value) => {
     }
 }
 
-const currencies = [dolarValue, euroValue, realValue];
+let printTable = () => {
+    console.log('Currency Converter');
+    console.log('-----------------------');
+    console.log('Peso Argentino: ', peso.toFixed(3));
+    console.log(`Dolar Oficial (${officialDolar}): ${(peso / officialDolar).toFixed(3)}`);
+    console.log(`Dolar Blue (${blueDolar}):  ${(peso / blueDolar).toFixed(3)}`);
+    console.log(`Real (${real}):  ${(peso / real).toFixed(3)}`);
+    console.log(`Euro (${euro}):  ${(peso / euro).toFixed(3)}`);
+}
+
+let printJson = () => {
+    var currencies = {
+        currencies: [
+            { name: 'ARS', value: peso.toFixed(3) },
+            { name: 'USD', value: (peso / officialDolar).toFixed(3) },
+            { name: 'USD-B', value: (peso / blueDolar).toFixed(3) },
+            { name: 'EUR', value: (peso / euro).toFixed(3) },
+            { name: 'BRL', value: (peso / real).toFixed(3) },
+        ]
+    };
+    console.log(currencies);
+}
+
+let printExchangeRate = () => {
+    var currencies = {
+        currencies: [
+            { name: 'USD', value: officialDolar.toFixed(3) },
+            { name: 'USD-B', value: blueDolar.toFixed(3) },
+            { name: 'EUR', value: euro.toFixed(3) },
+            { name: 'BRL', value: real.toFixed(3) },
+        ]
+    };
+    console.log(currencies);
+}
+
+const currencies = [getDolar, getEuro, getReal];
 
 Promise
     .all(currencies)
     .then(data => {
         [{ officialDolar, blueDolar }, euro, real] = data;
-        peso = pesoValue(currencyType, inputValue);
+        if (printFormat != 'C') {
+            peso = getPeso(currencyType, inputValue);
 
-        console.log('Currency Converter');
-        console.log('-----------------------');
-        console.log('Peso Argentino: ', peso.toFixed(3));
-        console.log(`Dolar Oficial (${officialDolar}): ${(peso / officialDolar).toFixed(3)}` );
-        console.log(`Dolar Blue (${blueDolar}):  ${(peso / blueDolar).toFixed(3)}`);
-        console.log(`Real (${real}):  ${(peso / real).toFixed(3)}`);
-        console.log(`Euro (${euro}):  ${(peso / euro).toFixed(3)}`);
-
+            if (printFormat === 'T') {
+                printTable();
+            } else {
+                printJson();
+            }
+        } else {
+            printExchangeRate();
+        }
     })
     .catch(err => console.log(err));
